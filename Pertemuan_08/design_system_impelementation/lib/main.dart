c82b947b4bb49902a1e948f6ac5331fc233769d9
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2024-12-05 11:49:35 +0700

    Add Flutter secure storage plugin support for macOS, Linux, and Windows

--- Pertemuan_08/design_system_impelementation/lib/main.dart
+++ Pertemuan_08/design_system_impelementation/lib/main.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
 import 'package:design_system_impelementation/design_system/styles/color_collections.dart';
 import 'package:design_system_impelementation/design_system/styles/spacing_collections.dart';
 import 'package:design_system_impelementation/design_system/styles/typography_collections.dart';
 import 'package:design_system_impelementation/design_system/widgets/button/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +22,44 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Alta - Design System Implementation'),
+      home: TransitionPage(),
+    );
+  }
+}
+
+class TransitionPage extends StatefulWidget {
+  const TransitionPage({super.key});
+
+  @override
+  State<TransitionPage> createState() => _TransitionPageState();
+}
+
+class _TransitionPageState extends State<TransitionPage> {
+  Future<bool> checkUserLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = prefs.getBool("isLogin") ?? false;
+    return isLogin;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: checkUserLogin(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          if (snapshot.data == true) {
+            return const ProfilePage();
+          } else {
+            return const MyHomePage(title: "Design System Implementation");
+          }
+        }
+      },
     );
   }
 }
@@ -32,6 +73,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String? name = "-";
+
+  @override
+  void initState() {
+    super.initState();
+    getName();
+  }
+
+  Future<void> getName() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      name = prefs.getString("name") ?? "-";
+      print("Data Local Name adalah $name");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,26 +102,145 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Hello World, Design System Implementation',
-              style: TypographyCollections.h1.copyWith(
-                color: ColorCollections.accentBlue,
-              ),
-              textAlign: TextAlign.center,
+              'Hello, $name',
+              style: TypographyCollections.h1,
             ),
-            const SizedBox(height: SpacingCollections.md),
-            Text(
-              "Caption: lorem ipsum dolor sit amet",
-              style: TypographyCollections.p1.copyWith(
-                color: ColorCollections.neutral500,
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                var storage = const FlutterSecureStorage();
+                await storage.write(key: "secure_name", value: name!);
+                setState(() {
+                  name = "Bambang Budiawan";
+                  prefs.setString("name", name!);
+                });
+              },
+              child: const Text(
+                "Set Name",
+                style: TypographyCollections.h2,
               ),
             ),
-            const SizedBox(height: SpacingCollections.md),
-            const CustomButton(
-              label: "Custom Button",
-              color: ColorCollections.accentBlue,
-              icon: Icon(Icons.add),
-              width: 200,
-              height: 50,
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                var storage = const FlutterSecureStorage();
+                String secure_name = await storage.read(key: "secure_name") ?? "-";
+                print("Secure Name adalah $secure_name");
+                setState(() {
+                  name = prefs.getString("name");
+                  print("Data Local Name adalah $name");
+                });
+              },
+              child: const Text("Get Name", style: TypographyCollections.h2),
+            ),
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  name = "-";
+                });
+              },
+              child: const Text("Clear Name", style: TypographyCollections.h2),
+            ),
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.setBool("isLogin", true);
+              },
+              child: const Text("Set Login", style: TypographyCollections.h2),
+            ),
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.setBool("isLogin", false);
+              },
+              child: const Text("Clear Data Login", style: TypographyCollections.h2),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Design System Implementation"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Anda Berhasil Login!',
+              style: TypographyCollections.h1,
+            ),
+            const SizedBox(height: SpacingCollections.lg),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(64.0),
+                ),
+              ),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.setBool("isLogin", false);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(title: "Design System Implementation"),
+                  ),
+                );
+              },
+              child: const Text("Logout"),
             ),
           ],
         ),
